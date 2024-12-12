65a1c59dd377362c91ddc874943919a4e7e6036b
unknown <pyatlosaikumar@gmail.com>
2024-12-13 00:16:02 +0530

    task on DOM part-1

---
+++ index.js
@@ -0,0 +1,53 @@
+
+let division1 = document.getElementById("division1");
+division1.style.cursor="pointer";
+function eventClick(){
+    division1.style.backgroundColor="red";
+    division1.style.padding="20px";
+    division1.style.color="white";
+    division1.style.borderRadius="10px";
+    division1.style.boxShadow="10px 10px 10px rgba(0,0,0,0.3),-10px -10px 10px rgba(0,0,0,0.3)";
+    division1.style.textAlign="center";
+    division1.style.fontSize="40px";
+    division1.style.margin="20px"
+}
+
+
+let division2 = document.getElementById("division2");
+division2.style.cursor="pointer";
+function eventDblClick(){
+    division2.style.backgroundColor="green";
+    division2.style.padding="20px";
+    division2.style.color="red";
+    division2.style.borderRadius="10px";
+    division2.style.boxShadow="10px 10px 10px rgba(0,0,0,0.3),-10px -10px 10px rgba(0,0,0,0.3)";
+    division2.style.textAlign="center";
+    division2.style.fontSize="30px";
+    division2.style.margin="20px"
+}
+
+let division3 = document.getElementById("division3");
+division3.style.cursor="pointer";
+function eventHover(){
+    division3.style.backgroundColor="red";
+    division3.style.padding="20px";
+    division3.style.color="white";
+    division3.style.borderRadius="10px";
+    division3.style.boxShadow="10px 10px 10px rgba(0,0,0,0.3),-10px -10px 10px rgba(0,0,0,0.3)";
+    division3.style.textAlign="center";
+    division3.style.fontSize="30px";
+    division3.style.margin="20px"
+}
+
+let division4 = document.getElementById("division3");
+division4.style.cursor="pointer";
+function eventOffHover(){
+    division4.style.backgroundColor="";
+    division4.style.padding="";
+    division4.style.color="";
+    division4.style.borderRadius="";
+    division4.style.boxShadow="";
+    division4.style.textAlign="";
+    division4.style.fontSize="";
+    division4.style.margin="";
+}
